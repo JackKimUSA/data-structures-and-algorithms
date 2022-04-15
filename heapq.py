bfbf4649fe07d3b636cd98f81c924d5583dc43f9
Jack Kim <jackkimusa@gmail.com>
2022-04-15 01:57:24 -0700

    Updated python file

--- heapq.py
+++ heapq.py
@@ -27,8 +27,8 @@ print(hq)
 heapq.heappop(hq)
 print(hq)
 print('')
-# Max heapq
 
+# Max heapq
 heap_items=[3,5,4,1,2]
 min_heap =[3,5,4,1,2]
 heapq.heapify(min_heap)
@@ -41,5 +41,55 @@ print('Max Heap:', max_heap)
 
 max_item = heapq.heappop(max_heap)[1]
 print(max_item)
-    
 
+# Max heapq
+for i in range(len(heap_items)):
+    heap_items[i] *= -1
+print(heap_items)
+heapq.heapify(heap_items)
+print(heap_items)
+for i in range(len(heap_items)):
+    heap_items[i] *= -1
+print(heap_items)
+
+# Min heapq
+heap_item=[2,7,4,1,8,1]
+hq=[]
+heapq.heappush(hq,2)
+print(hq)
+heapq.heappush(hq,7)
+print(hq)
+heapq.heappush(hq,4)
+print(hq)
+heapq.heappush(hq,1)
+print(hq)
+heapq.heappush(hq,8)
+print(hq)
+heapq.heappush(hq,1)
+print(hq)
+print('')
+
+# Max heapq
+hq=[]
+heapq.heappush(hq,-2)
+print(hq)
+heapq.heappush(hq,-7)
+print(hq)
+heapq.heappush(hq,-4)
+print(hq)
+heapq.heappush(hq,-1)
+print(hq)
+heapq.heappush(hq,-8)
+print(hq)
+heapq.heappush(hq,-1)
+print(hq)
+heapq.heappop(hq)
+print(hq)
+heapq.heappop(hq)
+print(hq)
+heapq.heappop(hq)
+print(hq)
+heapq.heappop(hq)
+print(hq)
+heapq.heappop(hq)
+print(hq)
